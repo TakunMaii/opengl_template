5de5e9f4eb37ab192802c0d30e4a061ba52a2818
maii <maii_wzy@163.com>
2026-05-20 23:47:44 +0800

    macos 适配

--- makefile
+++ makefile
@@ -5,17 +5,65 @@ ROOT_SOURCE=src/main.c src/glad.c
 SOURCE=
 
 CC=gcc
-CFLAGS=-Wall -Wextra -I external/include -L external/lib -lglfw3dll
+UNAME_S := $(shell uname -s)
+UNAME_M := $(shell uname -m)
+
+CPPFLAGS := -I external/include
+CFLAGS := -Wall -Wextra
+LDFLAGS :=
+LDLIBS :=
+RUNTIME_LIB :=
+
+ifeq ($(UNAME_S),Darwin)
+ifeq ($(UNAME_M),arm64)
+MACOS_LIB_DIR := external/lib/macos/lib-arm64
+else ifeq ($(UNAME_M),x86_64)
+MACOS_LIB_DIR := external/lib/macos/lib-x86_64
+else
+MACOS_LIB_DIR := external/lib/macos/lib-universal
+endif
+
+LDFLAGS += -L $(MACOS_LIB_DIR)
+LDLIBS += -lglfw3
+LDLIBS += -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo -framework CoreFoundation
+RUNTIME_LIB := $(MACOS_LIB_DIR)/libglfw.3.dylib
+else ifneq (,$(findstring MINGW,$(UNAME_S)))
+LDFLAGS += -L external/lib
+LDLIBS += -lglfw3dll -lgdi32
+RUNTIME_LIB := external/lib/glfw3.dll
+else ifneq (,$(findstring MSYS,$(UNAME_S)))
+LDFLAGS += -L external/lib
+LDLIBS += -lglfw3dll -lgdi32
+RUNTIME_LIB := external/lib/glfw3.dll
+else ifeq ($(UNAME_S),Linux)
+LDFLAGS += -L external/lib
+LDLIBS += -lglfw3 -lGL -lm -lpthread -ldl
+LDLIBS += -lX11 -lXrandr -lXi -lXxf86vm -lXcursor
+else
+$(error Unsupported platform: $(UNAME_S))
+endif
 
 TARGET=build/main
+BUILD_ARTIFACTS := $(TARGET)
 
+ifneq ($(strip $(RUNTIME_LIB)),)
+RUNTIME_COPY := build/$(notdir $(RUNTIME_LIB))
+BUILD_ARTIFACTS += $(RUNTIME_COPY)
+endif
 
 all: run
 
-build: $(TARGET)
+build: $(BUILD_ARTIFACTS)
 
-run: $(TARGET)
+run: build
 	./$(TARGET)
 
 $(TARGET): $(HEADER) $(ROOT_SOURCE) $(SOURCE)
-	$(CC) $(ROOT_SOURCE) $(SOURCE) -o $(TARGET) $(CFLAGS)
+	mkdir -p $(dir $@)
+	$(CC) $(CPPFLAGS) $(CFLAGS) $(ROOT_SOURCE) $(SOURCE) -o $(TARGET) $(LDFLAGS) $(LDLIBS)
+
+ifneq ($(strip $(RUNTIME_COPY)),)
+$(RUNTIME_COPY): $(RUNTIME_LIB)
+	mkdir -p $(dir $@)
+	cp $(RUNTIME_LIB) $(RUNTIME_COPY)
+endif
--- src/main.c
+++ src/main.c
@@ -3,13 +3,52 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+static void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
+    (void)window;
     glViewport(0, 0, width, height);
 }
 
-int main(int argc, char** argv)
+static bool compile_shader(GLuint shader, const char *source)
 {
+    int success;
+    char info_log[512];
+
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, sizeof(info_log), NULL, info_log);
+        printf("shader compile failed: %s\n", info_log);
+        return false;
+    }
+
+    return true;
+}
+
+static bool link_program(GLuint program)
+{
+    int success;
+    char info_log[512];
+
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(program, sizeof(info_log), NULL, info_log);
+        printf("program link failed: %s\n", info_log);
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
 	glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -39,6 +78,13 @@ int main(int argc, char** argv)
         printf("glad initialize failed\n");
         return -1;
     }
+
+    {
+        int framebuffer_width;
+        int framebuffer_height;
+        glfwGetFramebufferSize(window, &framebuffer_width, &framebuffer_height);
+        glViewport(0, 0, framebuffer_width, framebuffer_height);
+    }
     
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -48,6 +94,53 @@ int main(int argc, char** argv)
         -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
          0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
     };
+
+    const char *vertex_shader_source =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 vertexColor;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(aPos, 1.0);\n"
+        "    vertexColor = aColor;\n"
+        "}\n";
+
+    const char *fragment_shader_source =
+        "#version 330 core\n"
+        "in vec3 vertexColor;\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(vertexColor, 1.0);\n"
+        "}\n";
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    GLuint shader_program = glCreateProgram();
+
+    if (!compile_shader(vertex_shader, vertex_shader_source) ||
+        !compile_shader(fragment_shader, fragment_shader_source))
+    {
+        glDeleteShader(vertex_shader);
+        glDeleteShader(fragment_shader);
+        glfwTerminate();
+        return -1;
+    }
+
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    if (!link_program(shader_program))
+    {
+        glDeleteShader(vertex_shader);
+        glDeleteShader(fragment_shader);
+        glDeleteProgram(shader_program);
+        glfwTerminate();
+        return -1;
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -74,6 +167,7 @@ int main(int argc, char** argv)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shader_program);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -87,6 +181,7 @@ int main(int argc, char** argv)
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shader_program);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
